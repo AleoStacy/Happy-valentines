50179a346b29518d0435ad95268a54ab79a20878
stacyaleyo <aleyostacy@gmail.com>
2024-02-13 13:56:17 +0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,18 @@
+// let poll = {
+//     question: 'Yvonne My Love, Will You be My Valentine ? 🌷',
+//     answer:[
+//         'Yes', 'No', 'Maybe' 
+//     ],
+// pollCount: 100,
+// answerWeight: [ 50, 25, 25],
+// selectedAnswer: -1
+// };
+/* Container Styles */
+
+document.getElementById('yesBtn').addEventListener('click', function() {
+    alert('That\'s wonderful! Happy Valentine\'s Day Baby😍');
+});
+
+document.getElementById('noBtn').addEventListener('click', function() {
+    alert('Oh no! Maybe next time😓.');
+});
